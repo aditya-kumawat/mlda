17da35486b5038e78c4fe88ac854ad591e515c6e
aditya19496 <icm2014001@iiita.ac.in>
2019-01-03 13:07:53 +0530

    React setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './stylesheets/flex.scss';
+import './stylesheets/index.scss';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
